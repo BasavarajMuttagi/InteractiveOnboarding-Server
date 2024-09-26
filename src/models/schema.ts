441e551ae2dff083f4b5644d9ebe8996e1e0f78b
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-09-26 14:49:58 +0530

    add Question, Questionnaire, Attempt models

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -24,5 +24,92 @@ const userSchema = new Schema(
   { timestamps: true },
 );
 
+const questionSchema = new Schema(
+  {
+    question: {
+      text: {
+        type: String,
+        required: true,
+      },
+      subtext: {
+        type: String,
+        default: "",
+      },
+    },
+    options: {
+      type: [{ option: String, value: String }],
+    },
+    type: {
+      type: String,
+      enum: ["radio", "multiselect", "date"],
+      required: true,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const questionnaireSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    questions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Question",
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+const attemptSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    questionnaire: {
+      type: Schema.Types.ObjectId,
+      ref: "Questionnaire",
+      required: true,
+    },
+    attemptNumber: {
+      type: Number,
+      required: true,
+    },
+    responses: [
+      {
+        question: {
+          type: Schema.Types.ObjectId,
+          ref: "Question",
+          required: true,
+        },
+        answer: {
+          type: Schema.Types.Mixed,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+const Attempt = model("Attempt", attemptSchema);
+const Questionnaire = model("Questionnaire", questionnaireSchema);
+const Question = model("Question", questionSchema);
 const User = model("User", userSchema);
-export { User };
+export { User, Question, Questionnaire, Attempt };
