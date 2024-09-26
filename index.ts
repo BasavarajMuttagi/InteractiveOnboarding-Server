b2b2f0c55b758e69c7a3d2f33e26fd7e62a1ad20
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-09-26 16:22:01 +0530

    add db

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import AuthRouter from "./src/routes/auth.route";
+import { connect } from "mongoose";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 export const SECRET_SALT = process.env.SECRET_SALT as string;
+const DATABASE_URL = process.env.DATABASE_URL as string;
 app.use(cors());
 app.use(express.json());
 app.use("/auth", AuthRouter);
@@ -14,8 +16,18 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+async function main() {
+  try {
+    await connect(DATABASE_URL).then(() => {
+      app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
 
 export default app;
